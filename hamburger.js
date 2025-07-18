d0c2a6bbc93c2510a44d3e2360ea963d57ac902c
AK <adikraid1234@gmail.com>
2025-07-19 00:02:31 +0300

    added hamburger menu for mobile

---
+++ hamburger.js
@@ -0,0 +1,7 @@
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('#main-nav ul');
+
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('open');
+});
